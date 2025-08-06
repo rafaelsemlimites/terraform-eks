ffe869b85d848fc7ba5b3f92f6eccc44e6769ad5
Rafael <rafaelchaves.ti@gmail.com>
2025-08-06 14:12:48 -0300

    fix: lint

--- provider.tf
+++ provider.tf
@@ -1,9 +1,14 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "6.7.0"
     }
+  }
+  backend "s3" {
+    bucket = "comunidadedevops-s33"  # Nome do bucket S3 onde ficará o state
+    key    = "dev/terraform.tfstate" # Caminho/arquivo do state no bucket
+    region = "us-east-1"             # Região onde o bucket está localizado
   }
 }
 
