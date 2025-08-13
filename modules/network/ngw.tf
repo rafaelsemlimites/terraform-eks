2d2a9771d42c02a072433ddfdf30d2687035e3a7
Rafael <rafaelchaves.ti@gmail.com>
2025-08-13 18:38:30 -0300

    fix: terraform fmt

--- modules/network/ngw.tf
+++ modules/network/ngw.tf
@@ -1,7 +1,7 @@
 resource "aws_eip" "eks_ngw_eip_1a" {
   domain = "vpc"
   tags = merge(
-    local.tags,
+    var.tags,
     {
       Name = "${var.project_name}-eip-1a"
     }
@@ -11,7 +11,7 @@ resource "aws_eip" "eks_ngw_eip_1a" {
 resource "aws_eip" "eks_ngw_eip_1b" {
   domain = "vpc"
   tags = merge(
-    local.tags,
+    var.tags,
     {
       Name = "${var.project_name}-eip-1b"
     }
@@ -24,7 +24,7 @@ resource "aws_nat_gateway" "eks_ngw_1a" {
   subnet_id     = aws_subnet.eks_subnet_public_1a.id
 
   tags = merge(
-    local.tags,
+    var.tags,
     {
       Name                     = "${var.project_name}-ngw-1a",
       "kubernetes.io/role/elb" = 1
@@ -37,7 +37,7 @@ resource "aws_nat_gateway" "eks_ngw_1b" {
   subnet_id     = aws_subnet.eks_subnet_public_1b.id
 
   tags = merge(
-    local.tags,
+    var.tags,
     {
       Name                     = "${var.project_name}-ngw-1b",
       "kubernetes.io/role/elb" = 1
@@ -55,7 +55,7 @@ resource "aws_route_table" "eks_private_route_table_1a" {
   }
 
   tags = merge(
-    local.tags,
+    var.tags,
     {
       Name = "${var.project_name}-priv-route-table-1a"
     }
@@ -71,7 +71,7 @@ resource "aws_route_table" "eks_private_route_table_1b" {
   }
 
   tags = merge(
-    local.tags,
+    var.tags,
     {
       Name = "${var.project_name}-priv-route-table-1b"
     }
